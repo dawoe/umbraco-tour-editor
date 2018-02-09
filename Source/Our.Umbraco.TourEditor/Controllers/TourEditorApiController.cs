1e8107291c705e7c8b63c242b08c04eb456c3271
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-02-09 16:20:43 +0100

    Added tour api controller with a method to create a new tour file

---
+++ Source/Our.Umbraco.TourEditor/Controllers/TourEditorApiController.cs
@@ -0,0 +1,73 @@
+﻿namespace Our.Umbraco.TourEditor.Controllers
+{
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    using global::Umbraco.Core.IO;
+    using global::Umbraco.Core.Logging;
+    using global::Umbraco.Web.Editors;
+    using global::Umbraco.Web.Models;
+    using global::Umbraco.Web.Mvc;
+    using global::Umbraco.Web.WebApi;
+
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// The tour editor api controller.
+    /// </summary>
+    [PluginController(Constants.PluginName)]
+    public class TourEditorApiController : BackOfficeNotificationsController
+    {
+        /// <summary>
+        /// Creates a new tour file
+        /// </summary>
+        /// <param name="filename">
+        /// The filename.
+        /// </param>
+        /// <returns>
+        /// The <see cref="HttpResponseMessage"/>.
+        /// </returns>
+        [HttpPost]
+        public HttpResponseMessage CreateTourFile(string filename)
+        {
+            // filename may not empty
+            if (string.IsNullOrEmpty(filename))
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+
+            // can not contain invalid chars
+            if (filename.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)
+            {
+                return this.Request.CreateNotificationValidationErrorResponse("File name contains invalid characters");
+            }
+            
+            var toursFolder = Path.Combine(IOHelper.MapPath(SystemDirectories.Config), "BackOfficeTours");
+            
+
+            // file should not exist
+            var filePath = Path.Combine(toursFolder, filename + ".json");
+            if (File.Exists(filePath))
+            {
+                return this.Request.CreateNotificationValidationErrorResponse("A file with this name already exists");
+            }
+
+            try
+            {
+                var content = JsonConvert.SerializeObject(new BackOfficeTour[0]);
+
+                File.WriteAllText(filePath, content);
+
+                return this.Request.CreateNotificationSuccessResponse("Tour file created succesfully");
+            }
+            catch (Exception e)
+            {
+                this.Logger.Error<TourEditorApiController>("Error creating tour file", e);
+                return this.Request.CreateNotificationValidationErrorResponse("Error creating tour file");
+            }
+        }
+    }
+}
