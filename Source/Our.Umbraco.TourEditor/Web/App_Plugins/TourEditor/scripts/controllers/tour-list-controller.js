593b65af4ac26cd274ed2e381d01dc74a6ec5930
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-02-12 14:05:15 +0100

    Show a list of tours

---
+++ Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/controllers/tour-list-controller.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+
+    function TourListController($scope) {
+        var vm = this;
+
+        vm.tours = $scope.model;       
+    }
+
+    angular.module("umbraco").controller("Our.Umbraco.TourEditor.TourListController",
+        [
+            '$scope',            
+            TourListController
+        ]);
+
+})();
