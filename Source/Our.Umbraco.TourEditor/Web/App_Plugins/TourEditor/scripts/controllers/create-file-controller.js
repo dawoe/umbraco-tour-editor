d2236d41c6cea20f4b723b8908ab42dab91b2b10
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-02-12 08:53:40 +0100

    Added controller for created dialog

---
+++ Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/controllers/create-file-controller.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    "use strict";
+
+    function CreateFileController($scope, notificationsService, tourResource) {
+        var vm = this;
+
+        vm.filename = '';
+
+    }
+
+    angular.module("umbraco").controller("Our.Umbraco.TourEditor.CreateFileController",
+        [
+            '$scope',
+            'notificationsService',
+            'Our.Umbraco.TourEditor.TourResource',
+            CreateFileController
+        ]);
+
+})();
