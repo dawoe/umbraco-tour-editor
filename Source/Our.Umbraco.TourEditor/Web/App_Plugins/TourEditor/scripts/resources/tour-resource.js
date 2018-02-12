7a9cf2b8a65e0e00cac4d3bc7e0ef4d2adfa0578
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-02-12 07:56:48 +0100

    Added angular tour resource

---
+++ Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/resources/tour-resource.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    "use strict";
+
+    function TourResource($http, umbRequestHelper) {
+
+        var apiUrl = Umbraco.Sys.ServerVariables["Our.Umbraco.TourEditor"].TourEditorApi;
+
+        var resource = {
+            createTourFile: createTourFile
+        };
+
+        return resource;
+
+        function createTourFile(filename) {
+            var data = JSON.stringify({ filename: filename });
+
+            return umbRequestHelper.resourcePromise(
+                $http.post(apiUrl + "CreateTourFile", data),
+                "Failed creating tourfile"
+            );
+        };
+
+    }
+
+    angular.module("umbraco.resources").factory("Our.Umbraco.TourEditor.TourResource", TourResource);
+
+})();
