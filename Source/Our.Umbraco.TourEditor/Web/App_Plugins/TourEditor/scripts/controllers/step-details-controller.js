a4f1523249a113d40a1bc60ad4b9681ebf3cb2f4
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-02-15 19:04:37 +0100

    Added some more props for step details

--- Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/controllers/step-details-controller.js
+++ Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/controllers/step-details-controller.js
@@ -4,22 +4,26 @@
     function StepDetailsController($scope, eventsService, formHelper) {
         var vm = this;
         vm.step = null;
-        vm.stepIndex = -1;
+        vm.stepIndex = -1;       
         vm.form = null;
+        vm.isIntro = false;
 
         var evts = [];
 
         vm.properties = {
             'Title': { 'label': 'Title', 'description': 'Enter the title for this step', 'propertyErrorMessage': 'The title is a required field' },
-            'Group': { 'label': 'Group', 'description': 'Enter the group name for this tour', 'propertyErrorMessage': 'The  group name is a required field' },
-            'Alias': { 'label': 'Alias', 'description': 'Enter the alias for this tour', 'propertyErrorMessage': 'This is a required field' },
-            'Sections': { 'label': 'Sections', 'description': 'Select the sections needed for this tour' },
-            'Enabled': { 'label': 'Enable this setting' }
+            'Content': { 'label': 'Text', 'description': 'Enter the text for this step' },
+            'Type': { 'label': 'Is intro step', 'description': 'Check if it is a intro step' },
+            'Element': { 'label': 'Element', 'description': 'Enter the selector for the element you wish to highlight' },
+            'Event': { 'label': 'Event', 'description': 'Enter the event needed to trigger the next step eg. click' },
+            'PreventClick': { 'label': 'Prevent clicking', 'description': 'Check this if you want to prevent the user clicking the highlighted element' }
         };
 
         evts.push(eventsService.on("toureditor.editstep", function (name, arg) {
             vm.stepIndex = arg.stepIndex;
-            vm.step = $scope.model.tours[arg.tourIndex].steps[arg.stepIndex];            
+            vm.step = $scope.model.tours[arg.tourIndex].steps[arg.stepIndex];
+
+            vm.isIntro = vm.step.type === 'intro';
         }));
 
         
@@ -30,7 +34,17 @@
             }
         });
 
-        
+        $scope.$watch('vm.isIntro', function () {
+
+            if (vm.step) {
+                if (vm.isIntro) {
+                    vm.step.type = 'intro';
+                } else {
+                    vm.step.type = null;
+                }
+            }
+            
+        });
 
     }
 
