f66fe897b274af4f1b8ec34dd8813b784af1b2b3
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-03-05 11:59:35 +0100

    Added simple tour on how to create a tour file

---
+++ Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/backoffice/tours/scripts/tourname.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+    function TourNameController($scope) {
+        var vm = this;
+        var element = angular.element($scope.model.currentStep.element);
+        vm.error = false;
+        vm.initNextStep = initNextStep;
+        function initNextStep() {
+            if (element.val().toLowerCase() === 'create-dictionary') {
+                $scope.model.nextStep();
+            } else {
+                vm.error = true;
+            }
+        }
+    }
+    angular.module('umbraco').controller('Our.Umbraco.TourEditor.TourNameController', TourNameController);
+}());
