5021e22475324ee2f3842223d499788edaf2ecc3
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-03-08 16:15:40 +0100

    Maded the tourname view more flexible so it can be reused for other required inputs

---
+++ Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/backoffice/tours/scripts/requiredinput-controller.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+    function RequiredInputController($scope) {
+        var vm = this;
+        var element = angular.element($scope.model.currentStep.element);        
+        vm.error = false;
+        vm.initNextStep = initNextStep;
+        function initNextStep() {
+            if (element.val().toLowerCase() === $scope.model.currentStep.customProperties.input) {
+                $scope.model.nextStep();
+            } else {
+                vm.error = true;
+            }
+        }
+    }
+    angular.module('umbraco').controller('Our.Umbraco.TourEditor.RequiredInputController', RequiredInputController);
+}());
