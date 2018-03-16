4378030bda9e1bb017842c7117db3b23aa8e8bdd
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-03-16 17:57:12 +0100

    #4 added view and controller for element overlay picker

---
+++ Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/controllers/element-picker-overlay-controller.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    "use strict";
+
+    function ElementPickerOverlayController($scope) {
+        var vm = this;
+
+        
+    }
+
+
+    angular.module("umbraco").controller("Our.Umbraco.TourEditor.ElementPickerOverlayController",
+        [
+            '$scope',            
+            ElementPickerOverlayController
+        ]);
+
+})();
