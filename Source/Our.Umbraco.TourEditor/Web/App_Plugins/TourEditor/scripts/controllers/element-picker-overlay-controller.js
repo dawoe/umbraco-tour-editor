442e97e134ede214d631df38b05d4b4adb57c131
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-03-19 15:41:35 +0100

    #4 load all tree data based on selected sections

--- Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/controllers/element-picker-overlay-controller.js
+++ Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/controllers/element-picker-overlay-controller.js
@@ -1,10 +1,13 @@
 ﻿(function () {
     "use strict";
 
-    function ElementPickerOverlayController($scope) {
+    function ElementPickerOverlayController($scope, $q, treeResource) {
         var vm = this;
 
         vm.isLoading = true;
+        vm.promises = {};
+
+        // get sections in correct format for view
         vm.sections = _.map($scope.model.sections, function(x) {
             return {
                 "alias": x.alias,
@@ -14,6 +17,7 @@
             };
         });
 
+        // add sections tab
         vm.tabs = [{
             active: true,
             id: 1,
@@ -22,19 +26,54 @@
             items : vm.sections
         }];
 
+       
+
         function pickElement(eventElement) {           
             $scope.model.submit("[data-element='" + eventElement + "']");
         }
 
         vm.pickElement = pickElement;
-        
-        vm.isLoading = false;
+
+        // handle data when all promises are resolved
+        $q.all(vm.promises).then(function() {
+          
+            for (var key in vm.promises.trees) {
+                if (vm.promises.trees.hasOwnProperty(key)) {
+                    vm.promises.trees[key].then(function(data) {
+                       if (data.isContainer) {
+                           console.log(data);
+                       }
+                    });
+                }
+            }
+
+            vm.isLoading = false;
+        }); 
+                
+        function init() {
+
+            // store promises based on sections
+            vm.promises.trees = {};
+            for (var i = 0; i < vm.sections.length; i++) {
+                var alias = vm.sections[i].alias;
+
+               
+                vm.promises.trees["sectiontrees-" + alias] = treeResource.loadApplication({
+                    "section": alias,
+                    "isDialig": true
+                });
+            }            
+        }
+
+        init();
     }
 
 
     angular.module("umbraco").controller("Our.Umbraco.TourEditor.ElementPickerOverlayController",
         [
-            '$scope',            
+            '$scope',
+            '$q',
+            'treeResource',
             ElementPickerOverlayController
         ]);
 
