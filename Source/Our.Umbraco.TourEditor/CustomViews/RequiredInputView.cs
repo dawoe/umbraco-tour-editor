5dc669dee7618ef8eba4c2d3a86b6adb1bda63f0
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-08-31 08:54:04 +0200

    #20 added required input view

---
+++ Source/Our.Umbraco.TourEditor/CustomViews/RequiredInputView.cs
@@ -0,0 +1,12 @@
+﻿namespace Our.Umbraco.TourEditor.CustomViews
+{
+    using Our.Umbraco.TourEditor.Attributes;
+
+    /// <summary>
+    /// The required input view.
+    /// </summary>
+    [CustomView("Required Input", "/App_Plugins/TourEditor/backoffice/tours/views/requiredinput.html")]
+    public class RequiredInputView
+    {
+    }
+}
