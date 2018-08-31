1cb0f3d632c6acea9a6080cb8ef21cf3051a0b1f
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-08-31 09:03:15 +0200

    #20 added plugin manager extension to find all classes attributed with a attribute

---
+++ Source/Our.Umbraco.TourEditor/ObjectResolution/PluginManagerExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace Our.Umbraco.TourEditor.ObjectResolution
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    using global::Umbraco.Core;
+
+    using Our.Umbraco.TourEditor.Attributes;
+
+    /// <summary>
+    /// The plugin manager extensions.
+    /// </summary>
+    internal static class PluginManagerExtensions
+    {
+        /// <summary>
+        /// Resolves classes attributed with customview attribute
+        /// </summary>
+        /// <param name="pluginmanager">
+        /// The pluginmanager.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
+        internal static IEnumerable<System.Type> ResolveCustomViews(this PluginManager pluginmanager)
+        {
+            var assembly = Assembly.Load("Our.Umbraco.TourEditor");
+            return pluginmanager.ResolveAttributedTypes<CustomViewAttribute>(specificAssemblies: new List<Assembly> { assembly });
+        }
+    }
+}
