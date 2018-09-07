ebc125b3ae3cf62674069a23a0668c8ff4bd77d6
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-09-07 15:43:41 +0200

    #20 added custom property attribute to required input view class

--- Source/Our.Umbraco.TourEditor/CustomViews/RequiredInputView.cs
+++ Source/Our.Umbraco.TourEditor/CustomViews/RequiredInputView.cs
@@ -1,10 +1,12 @@
 ﻿namespace Our.Umbraco.TourEditor.CustomViews
 {
+    using Our.Umbraco.TourEditor.Attributes;
     using Our.Umbraco.TourEditor.Interfaces;
 
     /// <summary>
     /// The required input view.
     /// </summary>   
+    [CustomProperty("input", "Required input", 0, Description = "Enter the required input from the user")]
     public class RequiredInputView : ICustomView
     {
         /// <summary>
@@ -15,6 +17,6 @@
         /// <summary>
         /// Gets the view path.
         /// </summary>
-        public string ViewPath => "/App_Plugins/TourEditor/backoffice/tours/views/requiredinput.html";
+        public string ViewPath => "/App_Plugins/TourEditor/backoffice/tours/views/requiredinput.html";       
     }
 }
