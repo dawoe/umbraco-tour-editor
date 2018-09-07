c1f53cb84be97855c60d1e8ed02d75d05a84dd40
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-09-07 15:47:07 +0200

    #20 add extension method to get custom property attributes from class

---
+++ Source/Our.Umbraco.TourEditor/Extensions/CustomViewExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Our.Umbraco.TourEditor.Extensions
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    using Our.Umbraco.TourEditor.Attributes;
+    using Our.Umbraco.TourEditor.Interfaces;
+
+    /// <summary>
+    /// Custom view extensions.
+    /// </summary>
+    internal static class CustomViewExtensions
+    {
+        /// <summary>
+        /// Gets all the custom properties assigned to the class
+        /// </summary>
+        /// <param name="view">
+        /// The view.
+        /// </param>
+        /// <returns>
+        /// The <see cref="List{T}"/>.
+        /// </returns>
+        public static List<CustomPropertyAttribute> GetCustomProperties(this ICustomView view)
+        {           
+            return view.GetType().GetCustomAttributes<CustomPropertyAttribute>().ToList();            
+        }
+    }
+}
