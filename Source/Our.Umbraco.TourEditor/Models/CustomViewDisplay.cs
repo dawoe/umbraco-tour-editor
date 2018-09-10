330e6f883652f5857b70f8240e758ef36131dab2
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-09-10 08:27:51 +0200

    #20 added custom view display model

---
+++ Source/Our.Umbraco.TourEditor/Models/CustomViewDisplay.cs
@@ -0,0 +1,23 @@
+﻿namespace Our.Umbraco.TourEditor.Models
+{
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// The custom view display model
+    /// </summary>
+    [DataContract(Name = "customView")]
+    internal class CustomViewDisplay
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the view path.
+        /// </summary>
+        [DataMember(Name = "viewPath")]
+        public string ViewPath { get; set; }
+    }
+}
