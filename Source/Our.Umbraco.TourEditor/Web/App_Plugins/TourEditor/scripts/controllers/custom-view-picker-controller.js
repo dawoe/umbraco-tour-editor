e64eb071dbe145901d02bd349dab3ae0d26f088d
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-09-10 10:59:35 +0200

    #20 render custom properties after selecting a view

--- Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/controllers/custom-view-picker-controller.js
+++ Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/controllers/custom-view-picker-controller.js
@@ -7,20 +7,43 @@
         vm.isLoading = true;
         vm.customViews = [];
         vm.selectedView = $scope.model.selectedView;
+        vm.customProperties = [];
 
         if (!vm.selectedView) {
             vm.selectedView = '';
         }        
 
-        vm.properties = {
-            'CustomView': { 'label': 'View', 'description': 'Select the view you want to use'}            
-        };
+        vm.customViewProperty = { 'label': 'View', 'description': 'Select the view you want to use'};
 
         function changeView() {           
             $scope.model.selectedView = vm.selectedView;
+            setupCustomProperties();
         }
 
         vm.changeView = changeView;
+
+        function setupCustomProperties() {
+            vm.customProperties = [];
+
+            if (vm.selectedView !== '') {
+                var currentView = _.find(vm.customViews, function (view) { return view.viewPath === vm.selectedView; });               
+                if (currentView) {
+                    for (var i = 0; i < currentView.customProperties.length; i++) {
+                        var currentProp = currentView.customProperties[i];
+
+                        vm.customProperties.push({
+                            texts: { 'label': currentProp.label, 'description': currentProp.description },
+                            editor: {
+                                'view': currentProp.view,
+                                'config': currentProp.config,
+                                'value' : ''
+                            },
+                            property: currentProp.property
+                        });
+                    }                   
+                }
+            }
+        }
         
         function init() {
             tourResource.getCustomViews().then(function(views) {               
@@ -29,6 +52,8 @@
                     "name": "Select a view",
                     "viewPath": ""
                 });
+
+                setupCustomProperties();
 
                 vm.isLoading = false;
             });
