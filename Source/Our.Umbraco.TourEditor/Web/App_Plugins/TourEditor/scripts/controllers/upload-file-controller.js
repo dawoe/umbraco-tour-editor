92230e3ec6487259c22e92453b3a726a2da424ce
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-11-21 08:55:16 +0100

    #22 disable button until upload can be done

--- Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/controllers/upload-file-controller.js
+++ Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/controllers/upload-file-controller.js
@@ -4,10 +4,15 @@
     function UploadFileController($scope) {
         var vm = this;
 
+        vm.canUpload = false;
+
+        vm.rebuildUpload = 1;
+
         //listen for when a file is selected
         $scope.$on("filesSelected", function (event, args) {
             console.log(event);
             console.log(args);
+            vm.canUpload = true;
         });
     }
 
