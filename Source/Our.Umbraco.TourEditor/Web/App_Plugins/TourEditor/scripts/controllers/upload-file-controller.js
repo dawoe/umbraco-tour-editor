17e2f0be773de925ddba192905adb372759237d7
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-11-21 11:11:40 +0100

    #22 added validation on file extension for uploaded file

--- Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/controllers/upload-file-controller.js
+++ Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/controllers/upload-file-controller.js
@@ -1,24 +1,34 @@
 ﻿(function () {
     "use strict";
 
-    function UploadFileController($scope) {
+    function UploadFileController($scope, notificationsService) {
         var vm = this;
 
         vm.canUpload = false;
-
-        vm.rebuildUpload = 1;
+        vm.fileError = false;
 
         //listen for when a file is selected
         $scope.$on("filesSelected", function (event, args) {
-            console.log(event);
-            console.log(args);
-            vm.canUpload = true;
+            $scope.$apply(function() {
+                vm.canUpload = false;
+                vm.fileError = false;
+                if (args.files && args.files.length > 0) {
+                    var file = args.files[0];
+
+                    if (file.name.endsWith('.json')) {
+                        vm.canUpload = true;
+                    } else {
+                        vm.fileError = true;
+                    }
+                }
+            });
         });
     }
 
     angular.module("umbraco").controller("Our.Umbraco.TourEditor.UploadFileController",
         [
-            '$scope',           
+            '$scope', 
+            'notificationsService',
             UploadFileController
         ]);
 
