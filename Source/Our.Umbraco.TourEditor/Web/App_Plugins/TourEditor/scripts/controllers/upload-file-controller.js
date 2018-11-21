20a4fc25aad40eefae75a36ad7832f8a935a7857
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-11-21 08:29:05 +0100

    #22 added view and controller for upload action

---
+++ Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/controllers/upload-file-controller.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    "use strict";
+
+    function UploadFileController($scope) {
+        var vm = this;
+
+        //listen for when a file is selected
+        $scope.$on("filesSelected", function (event, args) {
+            console.log(event);
+            console.log(args);
+        });
+    }
+
+    angular.module("umbraco").controller("Our.Umbraco.TourEditor.UploadFileController",
+        [
+            '$scope',           
+            UploadFileController
+        ]);
+
+})();
