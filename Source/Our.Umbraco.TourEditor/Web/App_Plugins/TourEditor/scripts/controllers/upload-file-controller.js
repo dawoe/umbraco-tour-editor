fe1000ed47266e0f7f916b3d5292977beaa18fa5
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-11-27 10:42:27 +0100

    #22 added code to collect selected file on client for upload

--- Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/controllers/upload-file-controller.js
+++ Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/controllers/upload-file-controller.js
@@ -6,17 +6,25 @@
 
         vm.canUpload = false;
         vm.fileError = false;
+        vm.file = null;
+        vm.formData = null;
 
         //listen for when a file is selected
         $scope.$on("filesSelected", function (event, args) {
             $scope.$apply(function() {
                 vm.canUpload = false;
                 vm.fileError = false;
+                vm.file = null;
+                vm.formData = null;
                 if (args.files && args.files.length > 0) {
                     var file = args.files[0];
-
+                    
                     if (file.name.endsWith('.json')) {
                         vm.canUpload = true;
+                        vm.file = file;
+                        vm.formData = new FormData();
+                        vm.formData.append('file', file, file.name);
+                        console.log(vm.formData);
                     } else {
                         vm.fileError = true;
                     }
