8d003ae24d0c664450adff843d7995ef09ec33d3
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-12-11 13:57:21 +0100

    #24 register support of features in new versions of tours in server variables

--- Source/Our.Umbraco.TourEditor/UmbracoStartup.cs
+++ Source/Our.Umbraco.TourEditor/UmbracoStartup.cs
@@ -8,6 +8,7 @@
 
     using global::Umbraco.Core;
     using global::Umbraco.Web;
+    using global::Umbraco.Web.Models;
     using global::Umbraco.Web.UI.JavaScript;
 
     using Our.Umbraco.TourEditor.Controllers;
@@ -54,7 +55,16 @@
             var urlDictionairy = new Dictionary<string, object>();
 
             urlDictionairy.Add("TourEditorApi", urlHelper.GetUmbracoApiServiceBaseUrl<TourEditorApiController>(c => c.CreateTourFile(null)));
-            
+
+            var hasContentTypeProp = typeof(BackOfficeTour).GetProperty("ContentType") != null;
+
+            urlDictionairy.Add("SupportsContentType", hasContentTypeProp);
+
+            var hasCultureProp = typeof(BackOfficeTour).GetProperty("Culture") != null;
+
+            urlDictionairy.Add("SupportsCulture", hasCultureProp);
+
+
             if (!e.Keys.Contains("Our.Umbraco.TourEditor"))
             {
                 e.Add("Our.Umbraco.TourEditor", urlDictionairy);
