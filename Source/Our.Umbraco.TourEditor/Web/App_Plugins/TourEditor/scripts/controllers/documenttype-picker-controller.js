def9468d11006ded55cea2b7858285233d6799ed
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-12-11 14:39:09 +0100

    #24 added documenttype picker overlay view and controller

---
+++ Source/Our.Umbraco.TourEditor/Web/App_Plugins/TourEditor/scripts/controllers/documenttype-picker-controller.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    "use strict";
+
+    function DocumentTypePickerOverlayController($scope, $q) {
+        var vm = this;
+
+        vm.isLoading = true;
+       
+        function init() {
+           
+        }
+
+        init();
+    }
+
+
+    angular.module("umbraco").controller("Our.Umbraco.TourEditor.DocumentTypePickerOverlayController",
+        [
+            '$scope',
+            '$q',
+            DocumentTypePickerOverlayController
+        ]);
+
+})();
